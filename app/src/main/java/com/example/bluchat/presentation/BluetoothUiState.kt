1f3ded5b648aaa24c31280c28fa7e0db125d0d36
Lionel Emiliano Mazza <liomazza@gmail.com>
2023-03-29 08:29:21 -0300

    viewmodel, deviceScreen, di

---
+++ app/src/main/java/com/example/bluchat/presentation/BluetoothUiState.kt
@@ -0,0 +1,9 @@
+package com.example.bluchat.presentation
+
+import com.example.bluchat.domain.chat.BluetoothDevice
+
+data class BluetoothUiState(
+    val scannedDevices: List<BluetoothDevice> = emptyList(),
+    val pairedDevices: List<BluetoothDevice> = emptyList()
+) {
+}
